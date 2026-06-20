394065fef9ed7d2c7f01fc82c8354b01273d439c
Zorya Arghya <zoryasystemspvtltd@gmail.com>
2026-06-20 15:37:46 +0530

    Address property added in patient module

---
+++ LIS.DataModel/Migrations/202606200950114_patientaddress.cs
@@ -0,0 +1,18 @@
+﻿namespace LIS.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class patientaddress : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.PatientDetails", "Address", c => c.String(maxLength: 500));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.PatientDetails", "Address");
+        }
+    }
+}
